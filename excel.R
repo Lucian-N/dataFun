6f91a602bb3a8760c9c39efc3b10ca2863928130
Lucian-N <lucian_niculescu@ymail.com>
2018-09-24 20:19:03 +0300

    Excel files IO

--- excel.R
+++ excel.R
@@ -1,2 +1,17 @@
 #excel with R
 install.packages('readxl')
+
+#read from library 
+library(readxl)
+
+#read from an Excel file
+excel_sheets('Food log.xlsx')
+# assign to Data Frame
+df <- read_excel('Food log.xlsx', sheet = "Sheet1")
+# check df
+head(df)
+
+#sum cols
+sum(df$X__1)
+# stat quick
+summary(df)
